694b9bf70d4a32bba455d488e7fdcbaab5ab100a
Thiago <thiagojdb@tutanota.com>
2023-11-26 13:28:54 +0000

    adding a nice scroll animation to the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/Header'
+import Start from './pages/Start'
 
 function App() {
   return (
-    <>
+    <div class='relative'>
       <Header />
-    </>
+      <div class='pt-52'>
+        <Start/>
+      </div>
+    </div>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,23 @@
 import solid_logo from "../assets/solid.svg";
 import instagram_logo from "../assets/instagram-logo.svg"
 import shopping_cart_icon from "../assets/shopping-cart-outline-svgrepo-com.svg"
+import { createSignal } from "solid-js";
+import clsx from "clsx";
 
 function Header() {
+  let hide_offset = 300;
+
+  let [get_should_hide, set_should_hide] = createSignal(false);
+  const listener = document.addEventListener('scroll', (event) => {
+    const current_scroll = window.scrollY || document.documentElement.scrollTop;
+    if (current_scroll > hide_offset) {
+      set_should_hide(true);
+    } else {
+      set_should_hide(false);
+    }
+  });
   return (
-    <div class="fixed top-0 w-full flex justify-center shadow-md">
+    <div class={clsx('fixed h-52 top-0 w-full flex justify-center shadow-md z-40 bg-white transition-all duration-300', get_should_hide() ? '-top-52' : '')}>
       <div class="flex flex-col justify-center w-2/5">
         <div class="flex justify-center my-5">
           <img alt="Solid logo" src={solid_logo} class="h-32" />
